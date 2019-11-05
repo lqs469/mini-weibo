70746f4226f1e9ff0a540480f880cda1d66b8eea
lqs469 <liqinshuo469@gmail.com>
2019-11-05 23:39:12 +0800

    WIP

--- src/mini-weibo.js
+++ src/mini-weibo.js
@@ -1,1 +1,50 @@
 import './mini-weibo.css';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const init = (checkFn, cb) => {
+    if (checkFn()) {
+      cb();
+      return;
+    } else {
+      setTimeout(() => {
+        init(checkFn, cb);
+      }, 200);
+    }
+  }
+  
+  init(() => {
+    const searchPlaceholder = document.querySelector('.gn_search_v2 .placeholder');
+    return searchPlaceholder;
+  }, () => {
+    const searchPlaceholder = document.querySelector('.gn_search_v2 .placeholder');
+    const searchInput = document.querySelector('[node-type="searchInput"]');
+
+    searchPlaceholder.style.display = 'block';
+    searchPlaceholder.innerText = '微博搜索';
+    searchInput.addEventListener('focus', (event) => {
+      if (searchPlaceholder) {
+        searchPlaceholder.innerText = '';
+      }
+    });
+    
+    searchInput.addEventListener('blur', (event) => {
+      if (searchPlaceholder) {
+        searchPlaceholder.innerText = '微博搜索';
+      }
+    });
+  });
+
+  init(() => {
+    const headpic = document.querySelector('.headpic');
+    return headpic;
+  }, () => {
+    const headpic = document.querySelector('.headpic');
+
+    const newHeadpic = document.createElement('div');
+    newHeadpic.innerHTML = headpic.innerHTML;
+    newHeadpic.className = 'custom-headpic';
+
+    document.body.appendChild(newHeadpic)
+  })
+});
+
